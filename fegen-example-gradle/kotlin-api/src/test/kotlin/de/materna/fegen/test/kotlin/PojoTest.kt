ae8a071da6c498105e4036ab561ac820e0aa6b65
Clemens Classen <clemens.classen@materna.de>
2020-11-09 16:26:40 +0100

    Add cyclic typed pojo to backend example

--- fegen-example-gradle/kotlin-api/src/test/kotlin/de/materna/fegen/test/kotlin/PojoTest.kt
+++ fegen-example-gradle/kotlin-api/src/test/kotlin/de/materna/fegen/test/kotlin/PojoTest.kt
@@ -1,9 +1,27 @@
+/**
+ * Copyright 2020 Materna Information & Communications SE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package de.materna.fegen.test.kotlin
 
-import de.materna.fegen.example.gradle.kotlin.api.ComplexTestPojo
-import de.materna.fegen.example.gradle.kotlin.api.CreateRequest
-import de.materna.fegen.example.gradle.kotlin.api.PrimitiveTestPojo
-import de.materna.fegen.example.gradle.kotlin.api.RecursiveTestPojo
+import de.materna.fegen.example.gradle.kotlin.api.*
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
 
@@ -67,6 +85,12 @@ class PojoTest : ApiSpec() {
             val body = RecursiveTestPojo(null)
             val result = apiClient().testRestControllerClient.recursivePojo(body)
             result shouldBe RecursiveTestPojo(RecursiveTestPojo(null))
+        }
+
+        "cyclic pojo as body and return type" {
+            val body = CyclicTestPojoA(CyclicTestPojoB(null))
+            val result = apiClient().testRestControllerClient.cyclicPojo(body)
+            result shouldBe CyclicTestPojoA(CyclicTestPojoB(body))
         }
     }
 }
